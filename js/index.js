96e0125067d4d5d4f66161e1940b0cd733290cd8
Vuk Jovanovic <vukjovanovic34@gmail.com>
2020-02-26 19:03:08 +0100

    fixed header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
+import _ from 'underscore';
 import { openMenu, closeMenu } from './animations';
+
+// fixed header functionality
+const checkHeader = _.throttle(() => {
+  // Detect scroll position
+  let scrollPosition = Math.round(window.scrollY);
+
+  // If we scrolled we add class to our navigation bar
+  if (scrollPosition > 1) {
+    document.querySelector('.header').classList.add('headerFixed');
+  } else {
+    document.querySelector('.header').classList.remove('headerFixed');
+  }
+}, 300);
+
+window.addEventListener('scroll', checkHeader);
 
 // sidebar functionality on mobile devices
 const mobileBurger = document.querySelector('.mobileNavigation__burger');
